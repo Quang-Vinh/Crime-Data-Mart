702686ac81b535d8acef916e0a7671c15ff06e10
Vinh <qvinh@live.ca>
2020-02-22 13:18:21 -0500

    Restructured files

---
+++ 2 - Data Staging/SQL Scripts/view_tables.sql
@@ -0,0 +1,8 @@
+set schema 'CrimeDataMart';
+
+select * from date_dimension limit 10;
+select * from location_dimension limit 10;
+select * from crime_dimension limit 10;
+select * from event_dimension limit 10;
+select * from crime_fact limit 10;
+
