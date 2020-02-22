f0e4f1c9a9ab2bcc4acd6c32cc111eaa5aeef62e
Vinh <qvinh@live.ca>
2020-02-22 13:19:59 -0500

    Removed old files

--- 2 - Data Staging/view_tables.sql
+++
@@ -1,8 +0,0 @@
-set schema 'CrimeDataMart';
-
-select * from date_dimension limit 10;
-select * from location_dimension limit 10;
-select * from crime_dimension limit 10;
-select * from event_dimension limit 10;
-select * from crime_fact limit 10;
-
